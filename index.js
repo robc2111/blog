0e2b0d028f6ad9684b12a79c80e9c31e1f5d8939
robc2111 <rob.carney1@outlook.com>
2025-06-09 14:36:43 +0100

    create structure and some style

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;  
@@ -9,9 +16,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
-}
-);
+  const dataPath = path.join(__dirname, 'data/posts.json');
+  const posts = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  res.render('index.ejs', { posts });
+});
 
 app.post('/submit', (req, res) => {
     const { name, email, message } = req.body;
